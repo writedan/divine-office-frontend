4b36108b3b1484e4304583d5c4f75fac2d3f2ba1
Daniel Write <daniel@writefamily.com>
2024-12-05 17:02:05 -0500

    added listeners

--- src/electron.d.ts
+++ src/electron.d.ts
@@ -8,6 +8,10 @@ declare global {
       isCargoInstalled: () => boolean;
       installCargo: (targetTriple: string) => { success: boolean; error?: string };
 
+      // c.f. utils/message-utils.js
+      on: (event: string, listener: (...args: any[]) => void) => void;
+      removeListener: (event: string, listener: (...args: any[]) => void) => void;
+
       // utils/git-utils.js
       updateRepo: (repoPath: string, dirPath: string, branch?: string) => { success: boolean; error?: string };
 
