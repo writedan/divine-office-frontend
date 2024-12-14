79c529f38c7cd24e076bf19e6c2cda069bd64a4c
Daniel Write <daniel@writefamily.com>
2024-12-14 01:30:51 -0500

    added file exists

--- utils/url-utils.js
+++ utils/url-utils.js
@@ -1,5 +1,21 @@
-const { shell, ipcMain } = require("electron");
+const { ipcMain } = require('electron');
+const fs = require('fs');
+const path = require('path');
+const { app } = require('electron');
+
+ipcMain.handle('file-exists', async (_event, uri) => {
+  const basePath = app.getPath('userData');
+  const fullPath = path.join(basePath, uri);
+
+  try {
+    await fs.promises.access(fullPath, fs.constants.F_OK);
+    return true; 
+  } catch (error) {
+    return false; 
+  }
+});
+
 
 ipcMain.handle('open-link', (_event, url) => {
   shell.openExternal(url);
-})
+});
