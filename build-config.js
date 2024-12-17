1528859316fa1aa0e348fdb316e2380c30bbef6f
Daniel Write <daniel@writefamily.com>
2024-12-17 17:08:39 -0500

    fixed building for macos intel

---
+++ build-config.js
@@ -0,0 +1,36 @@
+module.exports = {
+  appId: "io.github.writedan.divineoffice",
+  files: [
+    "web-build/**/*",
+    "web-build/assets/**/*",
+    "node_modules/**/*",
+    "package.json",
+    "main.js",
+    "utils/**/*",
+    "preload.js",
+    "assets/**/*",
+    "APP_ROOT.js",
+    "**/*"
+  ],
+  extraMetadata: {
+    main: "main.js"
+  },
+  linux: {
+    target: [
+      "AppImage",
+      "deb"
+    ],
+    artifactName: "${productName}-${version}-linux-${platform}-${arch}.${ext}"
+  },
+  win: {
+    target: "nsis",
+    artifactName: "${productName}-${version}-win-installer-${platform}-${arch}.exe"
+  },
+  mac: {
+    artifactName: "${productName}-${version}-mac-${platform}-${arch}.${ext}"
+  },
+  asarUnpack: [
+    "**/node_modules/npm/**/*",
+    "**/node_modules/node/**/*"
+  ]
+};
